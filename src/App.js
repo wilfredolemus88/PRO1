22873b6232e1d0e3b929b69960cf80866dc0e87c
Wilfredo Lemus <wilfredolemus72@gmail.com>
2024-05-17 12:59:53 -0400

    This is the first REACT commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to to see what I change.
         </p>
+        <p>
+           This is where my first REACT will take place.
+        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"

8adb54f04cb4e7f1cd2c87f39a6eb40e899d298a
Wilfredo Lemus <wilfredolemus72@gmail.com>
2024-05-19 17:54:45 -0400

    This is the first App commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 
 function App() {
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to to see what I change.
-        </p>
-        <p>
-           This is where my first REACT will take place.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
